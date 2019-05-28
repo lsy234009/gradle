ca9d27c2e7737f27760e78d717ac29d82824e703
Adam Murdoch <adam@gradle.com>
2019-05-28 11:20:14 +1000

    Samples fixes.

--- subprojects/docs/src/samples/userguide/plugins/nestedObjects/java/buildSrc/src/main/java/DownloadPlugin.java
+++ subprojects/docs/src/samples/userguide/plugins/nestedObjects/java/buildSrc/src/main/java/DownloadPlugin.java
@@ -3,6 +3,6 @@ import org.gradle.api.Project;
 
 public class DownloadPlugin implements Plugin<Project> {
     void apply(Project project) {
-        project.getExtenions().create('download', DownloadExtension.class);
+        project.getExtenions().create("download", DownloadExtension.class);
     }
 }
--- subprojects/docs/src/samples/userguide/plugins/nestedObjects/kotlin/buildSrc/src/main/kotlin/DownloadPlugin.kt
+++ subprojects/docs/src/samples/userguide/plugins/nestedObjects/kotlin/buildSrc/src/main/kotlin/DownloadPlugin.kt
@@ -3,6 +3,6 @@ import org.gradle.api.Plugin
 
 class DownloadPlugin : Plugin<Project> {
     fun apply(project: Project) {
-        project.extensions.create('download', DownloadExtension::class.java)
+        project.extensions.create("download", DownloadExtension::class.java)
     }
 }
